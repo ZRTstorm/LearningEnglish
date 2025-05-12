4257110bf8a71f9182466c5e0c52912ddbb84740
ZRTstorm <blu3jean@naver.com>
2025-05-12 11:26:30 +0900

    Qdrant 삽입 조회 서비스 구현

--- spring-server/src/main/java/com/eng/spring_server/controller/EmbeddingController.java
+++ spring-server/src/main/java/com/eng/spring_server/controller/EmbeddingController.java
@@ -3,10 +3,9 @@ package com.eng.spring_server.controller;
 import com.eng.spring_server.service.EmbeddingService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.embedding.EmbeddingResponse;
-import org.springframework.ai.openai.api.OpenAiApi;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -37,5 +36,21 @@ public class EmbeddingController {
 
         Map<String, float[]> response = Collections.singletonMap("vectorValue", embeddingVector);
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @Operation(summary = "콘텐츠 벡터 저장", description = "콘텐츠의 벡터 데이터를 벡터 스토어에 저장 한다")
+    @PostMapping("/vectorStore/{contentType}/{contentId}")
+    public ResponseEntity<?> storeVector(@PathVariable String contentType, @PathVariable Long contentId) {
+        embeddingService.saveVector(contentType, contentId);
+
+        return ResponseEntity.status(HttpStatus.OK).body("OK");
+    }
+
+    @Operation(summary = "유사 콘텐츠 조회", description = "콘텐츠와 유사한 콘텐츠를 검색 한다")
+    @GetMapping("search/{contentType}/{contentId}")
+    public ResponseEntity<?> searchVector(@PathVariable String contentType, @PathVariable Long contentId) {
+        List<Document> documents = embeddingService.searchVector(contentType, contentId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(documents);
     }
 }
--- spring-server/src/main/java/com/eng/spring_server/service/EmbeddingService.java
+++ spring-server/src/main/java/com/eng/spring_server/service/EmbeddingService.java
@@ -1,18 +1,31 @@
 package com.eng.spring_server.service;
 
+import com.eng.spring_server.domain.contents.VectorContent;
+import com.eng.spring_server.repository.VectorContentRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.embedding.EmbeddingResponse;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.filter.FilterExpressionBuilder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmbeddingService {
 
     private final EmbeddingModel embeddingModel;
+    private final VectorStore vectorStore;
     private final TextOperationService textOperationService;
+    private final VectorContentRepository vectorContentRepository;
 
     public float[] getEmbeddingVector(String contentType, Long contentId) {
         // Get Summary Text for Content
@@ -21,5 +34,39 @@ public class EmbeddingService {
         EmbeddingResponse embResponse = embeddingModel.embedForResponse(List.of(summaryText));
 
         return embResponse.getResult().getOutput();
+    }
+
+    public void saveVector(String contentType, Long contentId) {
+        // Vector Value Checking
+        Optional<VectorContent> vectorOpt = vectorContentRepository.findByContentTypeAndContentId(contentType, contentId);
+        if (vectorOpt.isPresent()) {
+            log.info("Vector Content is Already in Store : {}", contentId);
+            throw new IllegalStateException("Vector Already Stored");
+        }
+        // Get Summary Text for Content
+        String summaryText = textOperationService.getSummaryText(contentType, contentId);
+
+        Map<String, Object> metaData = new HashMap<>();
+        metaData.put("contentType", contentType);
+        metaData.put("contentId", String.valueOf(contentId));
+
+        List<Document> documents = List.of(new Document(summaryText, metaData));
+        vectorStore.add(documents);
+
+        VectorContent content = new VectorContent();
+        content.setContentType(contentType);
+        content.setContentId(contentId);
+        vectorContentRepository.save(content);
+    }
+
+    public List<Document> searchVector(String contentType, Long contentId) {
+        // Get Summary Text for Content
+        String summaryText = textOperationService.getSummaryText(contentType, contentId);
+
+        FilterExpressionBuilder b = new FilterExpressionBuilder();
+
+        return vectorStore.similaritySearch(SearchRequest.builder().query(summaryText)
+                .topK(2)
+                .filterExpression(b.eq("contentType", contentType).build()).build());
     }
 }
