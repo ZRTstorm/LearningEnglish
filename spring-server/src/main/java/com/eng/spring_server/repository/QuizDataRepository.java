8a417fceedf2faf1c6f659c7e4eab358365b8683
ZRTstorm <blu3jean@naver.com>
2025-06-02 09:47:08 +0900

    콘텐츠 리스트 조회 구조 변경

--- spring-server/src/main/java/com/eng/spring_server/repository/QuizDataRepository.java
+++ spring-server/src/main/java/com/eng/spring_server/repository/QuizDataRepository.java
@@ -1,11 +1,19 @@
 package com.eng.spring_server.repository;
 
 import com.eng.spring_server.domain.contents.QuizData;
+import com.eng.spring_server.dto.CountAvgProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface QuizDataRepository extends JpaRepository<QuizData, Long> {
 
     List<QuizData> searchAllByContentsLibraryId(Long contentsLibraryId);
+
+    @Query("select count(q) as count, avg(q.score) as avg from QuizData q " +
+            "where q.contentsLibraryId = :contentsLibraryId")
+    CountAvgProjection findCountAndAvgScore(@Param("contentsLibraryId") Long contentsLibraryId);
+
 }
--- spring-server/src/main/java/com/eng/spring_server/repository/dictation/DictationListRepository.java
+++ spring-server/src/main/java/com/eng/spring_server/repository/dictation/DictationListRepository.java
@@ -1,7 +1,10 @@
 package com.eng.spring_server.repository.dictation;
 
 import com.eng.spring_server.domain.dictation.DictationList;
+import com.eng.spring_server.dto.CountAvgProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,5 +12,9 @@ import java.util.Optional;
 public interface DictationListRepository extends JpaRepository<DictationList, Long> {
     List<DictationList> findByContentsLibrary_Id(Long contentsLibraryId);
     Optional<DictationList> findBySentenceIdAndContentsLibrary_Id(Long sentenceId, Long contentsLibraryId);
+
+    @Query("select count(d) as count, avg(d.score) as avg from DictationList d " +
+            "where d.contentsLibrary.id = :contentsLibraryId")
+    CountAvgProjection findCountAndAverageScore(@Param("contentsLibraryId") Long contentsLibraryId);
 
 }
